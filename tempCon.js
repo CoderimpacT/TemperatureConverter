5360f8a86a8cf8bf1030973afb800acc9563007f
CoderimpacT <131013829+CoderimpacT@users.noreply.github.com>
2023-08-14 11:41:00 +0530

    Add files via upload

---
+++ tempCon.js
@@ -0,0 +1,19 @@
+function convertTemperature() {
+  const temperature = parseFloat(document.getElementById("temperature").value);
+  const unit = document.getElementById("unit").value;
+  let convertedTemperature = 0;
+
+  if (unit === "celsius") {
+    convertedTemperature = (temperature * 9) / 5 + 32;
+  } else if (unit === "fahrenheit") {
+    convertedTemperature = ((temperature - 32) * 5) / 9;
+  } else if (unit === "kelvin") {
+    convertedTemperature = temperature + 273.15;
+  }
+
+  document.getElementById(
+    "convertedTemperature"
+  ).innerText = `Converted Temperature: ${convertedTemperature.toFixed(
+    2
+  )} ${unit.toUpperCase()}`;
+}
